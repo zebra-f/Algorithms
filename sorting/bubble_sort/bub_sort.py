d0c1fe8d8d8810a3a9b5f5a1369dbbba5fe4115e
Rafal Zebrowski <rafal.zebrowski@gmail.com>
2021-04-17 20:10:22 +0200

    2nd method update

--- sorting/bubble_sort/bub_sort.py
+++ sorting/bubble_sort/bub_sort.py
@@ -6,9 +6,9 @@ def bubble_sort(array):
 
         for i in range(len(array) - 1):  # [2nd method] for i in range(len(array) - 1 - j):
             if array[i] > array[i + 1]:
-                a1 = array[i]
-                array[i] = array[i + 1]
-                array[i + 1] = a1
+                temp = array[i]  # [2nd method] array[i], array[i + 1] = array[i + 1], array[i]
+                array[i] = array[i + 1]  # [2nd method] delete this line
+                array[i + 1] = temp  # [2nd method] delete this line
                 did_it_swap = True
 
         if not did_it_swap:
--- sorting/bubble_sort/test_bub_sort.py
+++ sorting/bubble_sort/test_bub_sort.py
@@ -9,8 +9,11 @@ class TestBubbleSort(unittest.TestCase):
         self.assertEqual(bub_sort.bubble_sort([2, 1]), [1, 2])
         self.assertEqual(bub_sort.bubble_sort([1, -1]), [-1, 1])
         self.assertEqual(bub_sort.bubble_sort([0, -1]), [-1, 0])
+        self.assertEqual(bub_sort.bubble_sort([0, -1.5]), [-1.5, 0])
+        self.assertEqual(bub_sort.bubble_sort([0.2, -1]), [-1, 0.2])
         self.assertEqual(bub_sort.bubble_sort([4, 3, 2, 1]), [1, 2, 3, 4])
         self.assertEqual(bub_sort.bubble_sort([1, 2, 3, 4]), [1, 2, 3, 4])
+        self.assertEqual(bub_sort.bubble_sort([1, 2, 3.8, 4]), [1, 2, 3.8, 4])
         self.assertEqual(bub_sort.bubble_sort([7, 5, 3, -4, 2]), [-4, 2, 3, 5, 7])
 
 
